b476e7b397d245f6f6730a43a110fa1042ded2e1
jhernandezd95@gmail.com <jhernandezd95@gmail.com>
2022-10-25 20:30:47 -0400

    Creating user entity

---
+++ src/main/java/com/example/crud/modules/auth/entities/User.java
@@ -0,0 +1,86 @@
+package com.example.crud.modules.auth.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import java.util.Collection;
+import java.util.Date;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@AllArgsConstructor
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Email
+    @NotNull
+    @NotBlank
+    private String email;
+
+    private String password;
+
+    @Past
+    @NotNull
+    private Date birthDay;
+
+    @Past
+    private Date lastLogin;
+
+    private Boolean isEnable;
+
+    private Boolean isAccountNonLocked;
+
+    private Boolean isAccountNonExpired;
+
+    public User() {
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.isAccountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.isAccountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.isEnable;
+    }
+}
